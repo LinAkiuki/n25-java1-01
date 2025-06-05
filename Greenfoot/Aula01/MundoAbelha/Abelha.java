f2a02eb1dbb88df0f81d035f343fc63c605dcab6
Lineu Akiuki <linakiishi@gmail.com>
2025-06-04 21:51:52 -0300

    quanta mosca que tem aqui

--- Greenfoot/Aula01/MundoAbelha/Abelha.java
+++ Greenfoot/Aula01/MundoAbelha/Abelha.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Abelha here.
+ * Esta é uma Classe que reprensentará uma Abelha.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Lineu Akiuki 
+ * @version 20250604
  */
 public class Abelha extends Actor
 {
@@ -16,8 +16,11 @@ public class Abelha extends Actor
     {
         // Add your action code here.
         move(1);
-        if (Greenfoot.getKey()=="left"){
-            turn(-10);
+        if (Greenfoot.isKeyDown("left")){
+            turn(-5);
+        }
+        if (Greenfoot.isKeyDown("right")){
+            turn(5);
         }
     }
 }
