b9a14bc639fcdeca6424801b883a5dd5425a66cc
Lineu Akiuki <linakiishi@gmail.com>
2025-06-13 21:48:24 -0300

    Silva Sauro

--- Eclipse/Workspace/conversaoGalao2Litros/src/br/com/senaisp/bauru/classes/Estudante.java
+++ Eclipse/Workspace/conversaoGalao2Litros/src/br/com/senaisp/bauru/classes/Estudante.java
@@ -10,6 +10,12 @@ public class Estudante {
 	public Estudante() {
 		setNome("Sem nome");
 		ra = 0;
+		nota = 0.0;
+		ehFumante = false;
+	}
+	public Estudante(int ra, String nome) {
+		setRa(ra);
+		setNome(nome);
 		nota = 0;
 		ehFumante = false;
 	}
@@ -36,5 +42,13 @@ public class Estudante {
 	}
 	public void setEhFumante(boolean ehFumante) {
 		this.ehFumante = ehFumante;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Nome: " + getNome() + "\n" +
+			   "Ra: " + getRa() + "\n" +
+			   "Nota: "+ getNota() + "\n"+
+			   "Fumante: " + isEhFumante();
 	}
 }
